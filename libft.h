35469c8bd139a0dba289dc63e6e1506e1c8195b1
BrunoBMFT <bmmc136@gmail.com>
2026-05-12 19:48:36 +0100

    backup

--- libft.h
+++ libft.h
@@ -24,7 +24,6 @@ typedef struct s_list
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
-void	free_array(char **array);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
